0cb4fcfbd666d900c0281e823eb8be2c4b312324
Bazarin Ngigi <161435099+BazarinTech@users.noreply.github.com>
2025-11-27 07:44:14 +0300

    Update support email address in SupportBox component

--- src/components/SupportBox.tsx
+++ src/components/SupportBox.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function SupportBox() {
   return (
     <div className='w-full text-center'>
-        <p className="text-sm">Email us: support@trade-vault.live</p>
+        <p className="text-sm">Email us: support@trade-swing.tech</p>
     </div>
   )
 }
